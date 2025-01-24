2166459eaefcefa25c5554dbc2fa55943cded3b2
Salim Maalouf <salim.maalouf@mymonty.com>
2025-01-24 10:34:38 +0200

    first push

---
+++ MontyValidator/MontyValidator/MontyValidator.h
@@ -0,0 +1,18 @@
+//
+//  MontyValidator.h
+//  MontyValidator
+//
+//  Created by Salim Maalouf on 24/01/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MontyValidator.
+FOUNDATION_EXPORT double MontyValidatorVersionNumber;
+
+//! Project version string for MontyValidator.
+FOUNDATION_EXPORT const unsigned char MontyValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MontyValidator/PublicHeader.h>
+
+
---
+++ MontyValidator/MontyValidator/Validator.swift
@@ -0,0 +1,15 @@
+//
+//  Validator.swift
+//  MontyValidator
+//
+//  Created by Salim Maalouf on 24/01/2025.
+//
+
+import Foundation
+
+public class Validator {
+    
+    public static func getName(firstName: String, lastName: String) -> String {
+        "\(firstName + " " + lastName)"
+    }
+}
